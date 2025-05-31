8101fa08144ffe0af59f6beb20d9200c22210e1c
emianuelul <emanuel.marin0000@gmail.com>
2025-05-31 13:58:04 +0300

    feat: add resize canvas button

--- code.js
+++ code.js
@@ -1,19 +1,52 @@
 const canvas = document.querySelector(".canvas");
 const canvasSide = canvas.offsetWidth - 8;
 
+const resizeBtn = document.querySelector("#resizeBtn");
 let pixelCountW = 16;
 
+initCanvas(pixelCountW);
+let pixels = document.querySelectorAll(".pixel");
 
-for(let i = 0; i < pixelCountW; i++){
-    for(let j = 0; j < pixelCountW; j++){
-        let pixel = document.createElement("div");
-        pixel.classList.add(`${i}-${j}`);
-        pixel.classList.add("pixel");
-        pixel.style.width = `${canvasSide / pixelCountW}px`
-        pixel.style.height = `${canvasSide / pixelCountW}px`;
-        canvas.appendChild(pixel);
+// =============== FUNCTIONS =================
+function initCanvas(width){
+    for(let i = 0; i < width; i++){
+        for(let j = 0; j < width; j++){
+            let pixel = document.createElement("div");
+            pixel.classList.add(`${i}-${j}`);
+            pixel.classList.add("pixel");
+            pixel.style.width = `${canvasSide / width}px`;
+            pixel.style.height = `${canvasSide / width}px`;
+            canvas.appendChild(pixel);
+        }
     }
 }
 
-const pixels = document.querySelectorAll(".pixel");
-console.log(pixels)
+function setCanvasWidth(){
+    const newWidth = parseInt(prompt("What size x width do you want your canvas to be?"));
+    if(newWidth > 100){
+        newWidth = 100;
+    }
+
+    for(const pixel of pixels){
+        canvas.removeChild(pixel);
+    }
+    pixelCountW = newWidth;
+    initCanvas(pixelCountW);
+    pixels = document.querySelectorAll(".pixel");
+    console.log(pixels)
+}
+// =========== EVENT LISTENERS ===============
+
+canvas.addEventListener("click", (event) => {
+    const target = event.target;
+
+    target.style.backgroundColor = "black";
+})
+
+
+resizeBtn.addEventListener('click', () => setCanvasWidth());
+
+
+
+
+
