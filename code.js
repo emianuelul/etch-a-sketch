08a4e75e062ff5b363e6a9ca1a32b6cd18182271
emianuelul <emanuel.marin0000@gmail.com>
2025-05-31 17:54:12 +0300

    fix: fixed typo, now eraser works properly

--- code.js
+++ code.js
@@ -137,11 +137,11 @@ crazyCheck.addEventListener('change', () => {
 
 eraser.addEventListener('click', () => {
     if(eraserOn){
-        earserBtn.classList.remove('selected')
+        eraser.classList.remove('selected')
         eraserOn = false;
     }
     else{
-        earserBtn.classList.add('selected')
+        eraser.classList.add('selected')
         eraserOn = true;
     }
 })
